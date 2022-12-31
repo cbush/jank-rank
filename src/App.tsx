ab86a1b8376ef438db3aa3f59b0130d71fd74d76
Chris Bush <shim_insider.0j@icloud.com>
2022-12-31 00:37:30 -0500

    Set up jank rank

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { asyncMergeSort } from "./asyncMergeSort";
+import episodes from "./tng-episodes.json";
+
+type Episode = {
+  s: string;
+  e: string;
+  title: string;
+  summary: string;
+};
+
+type State = {
+  episodes?: Episode[];
+  a?: Episode;
+  b?: Episode;
+  selectA?(): void;
+  selectB?(): void;
+};
+
+function useSortEpisodes() {
+  const [state, setState] = useState<State | undefined>(undefined);
+
+  useEffect(() => {
+    let innerReject = (_: Error) => {};
+    asyncMergeSort(episodes, (a, b) => {
+      return new Promise((resolve, reject) => {
+        innerReject = reject;
+        setState({
+          a,
+          b,
+          selectA() {
+            resolve(-1);
+          },
+          selectB() {
+            resolve(1);
+          },
+        });
+      });
+    }).then((sorted) => {
+      setState({
+        episodes: sorted,
+      });
+    });
+    return () => {
+      innerReject(new Error("Tearing down"));
+    };
+  }, [setState]);
+
+  return state;
+}
 
 function App() {
+  const { a, b, selectA, selectB, episodes } = useSortEpisodes() ?? {};
+
+  if (!a || !b) {
+    return (
+      <>
+        <span>rank, season, episode, title</span>
+        <br />
+        {episodes?.map((e, i) => (
+          <>
+            <span key={e.title}>
+              {i + 1}, {e.s}, {e.e}, {e.title.replaceAll(",", "\\,")}
+            </span>
+            <br />
+          </>
+        ))}
+      </>
+    );
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Which is better?</h1>
+      <div className="buttons">
+        <button onClick={selectA}>
+          <h2>{a.title}</h2>
+          <p>
+            Season {a.s}, episode {a.e}
+          </p>
+          <p>{a.summary}</p>
+        </button>
+        <button onClick={selectB}>
+          <h2>{b.title}</h2>
+          <p>
+            Season {b.s}, episode {b.e}
+          </p>
+
+          <p>{b.summary}</p>
+        </button>
+      </div>
     </div>
   );
 }
